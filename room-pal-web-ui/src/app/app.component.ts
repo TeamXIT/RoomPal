e9b2f47333f29389c8f70622e36e25daaadb9cb1
chandrakommi <csdwt7613@gmail.com>
2024-06-22 15:33:59 +0000

    feat: custom validator usage

--- room-pal-web-ui/src/app/app.component.ts
+++ room-pal-web-ui/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ItemComponent } from './items/item/item.component';
+import { LoginComponent } from './modules/account/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ItemComponent],
+  imports: [RouterOutlet, LoginComponent, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- room-pal-web-ui/src/app/modules/account/login/login.component.ts
+++ room-pal-web-ui/src/app/modules/account/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { JsonPipe, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, JsonPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginFormGroup: FormGroup<{
+    mobileNumber: FormControl;
+    password: FormControl;
+  }> = new FormGroup({
+    mobileNumber: new FormControl('', [
+      Validators.required,
+      this.mobileNumberValidator,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  mobileNumberValidator(control: AbstractControl): ValidationErrors | null {
+    const value = new String(control.value).trim();
+    if (!value) {
+      return null;
+    }
+    if (value.length <= 1 && value.length < 10) return { minlength: true };
+    if (value.length > 10) return { maxlength: true };
+    return null;
+  }
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.loginFormGroup.markAllAsTouched();
+    // console.log(this.loginFormGroup.value);
+  }
 }
