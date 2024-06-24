28eaae6a1dce31da43347722217e5d1f566110a6
skayisha <ayisha7815@gmail.com>
2024-06-24 19:55:11 +0530

    removed scrcte keys

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,8 +2,8 @@ const { Cashfree } = require('cashfree-pg');
 const moment = require('moment');
 const Order = require('../models/paymentModel');
 const { baseResponses } = require('../helpers/baseResponses');
-Cashfree.XClientId = "TEST10123844e567d51edbee7c8a8cec44832101";
-Cashfree.XClientSecret = "cfsk_ma_test_42383a0c508319c6afbaaa8518324565_1b53e05d";
+Cashfree.XClientId = "";
+Cashfree.XClientSecret = "";
 Cashfree.XEnvironment = Cashfree.Environment.SANDBOX;
 
 const createOrder = async (req, res) => {
