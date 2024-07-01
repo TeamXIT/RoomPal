e1b026d068395a45c523b71608304d541fe82663
skayisha <ayisha7815@gmail.com>
2024-07-01 09:39:29 +0530

    baseResponses and constant messages added

--- backend/helpers/constantMessages.js
+++ backend/helpers/constantMessages.js
@@ -2,14 +2,16 @@ const BASE_RESPONSE_MESSAGES = {
     SUCCESS:{
         USER_REGISTERED: 'User registered successfully',
         USER_LOGGED_IN: 'User logged in successfully',
+        OTP_VERIFIED:'otp verified successfully',
         ROOM_CREATED_SUCCESSFULLY:'Room created successfully',
         RATING_GIVEN: 'Rating given successfully',
         ORDER_CREATED_SUCCESSFULLY: 'Order created successfully',
         ORDER_FETCHED_SUCCESSFULLY: 'Order fetched successfully',
         REFUND_CREATED: 'Refund created successfully',
         REFUND_FETCHED: 'Refund fetched successfully',
-        PAYMENT_CREATED: 'Payment created successfully'
-        PASSWORD_RESET_SUCCESS:"Password updated successfully"
+        PAYMENT_CREATED: 'Payment created successfully',
+        PASSWORD_RESET_SUCCESS:'Password updated successfully',
+        FEEDBACK:'Feedback updated successfully'
     },
     ERROR:{
         ALL_FIELDS_REQUIRED: 'All fields are required',
@@ -17,7 +19,8 @@ const BASE_RESPONSE_MESSAGES = {
         USER_NOT_FOUND: 'User not found',  
         INVALID_PASSWORD: 'Invalid password', 
         FEEDBACK_NOT_FOUND: 'Feedbacks are not given till now',
-        ORDER_NOT_FOUND: 'Order not found'
+        ORDER_NOT_FOUND: 'Order not found',
+        ROOM_NOT_FOUND: 'Room not found'
     }
 };
 module.exports = {BASE_RESPONSE_MESSAGES};
