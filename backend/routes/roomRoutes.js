81f422ae265a0151c922da2598aa5ef8a53ab388
skayisha <ayisha7815@gmail.com>
2024-07-05 16:53:00 +0530

    get room by id and name added in room controller

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { roomCreation, getAllRooms, updateRoomDetails} = require('../controllers/roomController');
+const { roomCreation, getAllRooms, updateRoomDetails, getRoomById, getRoomByName} = require('../controllers/roomController');
 const router = express.Router();
 router.post('/create',roomCreation);
 router.get('/getAll',getAllRooms);
 router.put('/update',updateRoomDetails);
-
+router.get('/getById',getRoomById);
+router.get('/getByName',getRoomByName);
 module.exports = router;
