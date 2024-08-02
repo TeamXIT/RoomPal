d43942feef513fc1ce6ef9ae903369525df967b4
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-08-02 13:02:04 +0530

    changes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createOrder, fetchOrder, webhook, getAllOrders } = require('../controllers/orderController');
+const { createOrder, fetchOrder, webhook, getAllOrders, getOrdersByCustomerId } = require('../controllers/orderController');
 const router = express.Router();
 router.post('/create-order',createOrder);
 router.get('/fetch-order',fetchOrder);
 router.post('/webhook',webhook);
 router.get('/getAll',getAllOrders);
+router.get('/getUsersOrders',getOrdersByCustomerId);
 module.exports = router;
